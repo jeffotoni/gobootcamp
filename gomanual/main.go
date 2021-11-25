d5bcc3973d73237c2b03f1a532dd22b1fe0b0726
jeffeotoni <jeff.otoni@gmail.com>
2021-11-25 08:59:02 -0300

    diretorio enxuto

--- gomanual/main.go
+++ gomanual/main.go
@@ -8,14 +8,9 @@ var (
 	HTTP_PORT = "0.0.0.0:8181"
 )
 
-////go:embed manual
-//var res embed.FS
-
 func main() {
 	mux := http.NewServeMux()
-	//fs := http.FileServer(http.FS(res))
-	//mux.Handle("/manual/", http.StripPrefix("/", fs))
-	mux.Handle("/manual/", http.StripPrefix("/manual",
+	mux.Handle("/", http.StripPrefix("/",
 		http.FileServer(http.Dir("./manual"))))
 	println("Run Server ", HTTP_PORT)
 	http.ListenAndServe(HTTP_PORT, mux)
