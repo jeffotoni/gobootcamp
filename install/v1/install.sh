ef0eb62d1ab99f55c8057486bc87a60ea8187b7d
jeffeotoni <jeff.otoni@gmail.com>
2021-11-26 03:33:56 -0300

    install correcao

--- install/v1/install.sh
+++ install/v1/install.sh
@@ -9,7 +9,7 @@ rm -f $DIR/bin/$EXEC
 wget -c "https://github.com/jeffotoni/gobootcamp/blob/main/install/v1/gobootcamp" -P "$DIR"
 echo "..."
 sleep 1
-chmod 755 -R $DIR/$EXEC
+chmod 755 $DIR/$EXEC
 
 if [ -e "$ZSHRC" ] ; then
 echo "\033[0;32m#########################################################\033[0m"
@@ -25,4 +25,5 @@ else
 fi
 
 echo "\033[0;33m######### Thanks for Download ##########\033[0m"
-echo "\033[0;33m You just need to type gobootcamp \033[0m"
+echo "\033[0;33m gobootcamp \033[0m"
+echo "......................................................"
